6d981ffc5c3fad3595de7ebaa4333b25a73e1966
Matéo Castella <castella.mateo@gmail.com>
2020-11-02 19:31:03 +0100

    changed platform and update scripts

--- multiplatform_post.sh
+++ multiplatform_post.sh
@@ -2,17 +2,14 @@
 
 cd /var/www/html/
 
-cp -r ./weam/weam/app ./weam-multiplatform/app
-cp -r ./weam/weam/public/css ./weam-multiplatform/public/css
-cp -r ./weam/weam/public/js ./weam-multiplatform/public/js
-cp -r ./weam/weam/resources ./weam-multiplatform/resources
-cp -r ./weam/weam/routes ./weam-multiplatform/routes
+cp -r ./weam/weam/app ./weam-multiplatform
+cp -r ./weam/weam/public/css ./weam-multiplatform/public
+cp -r ./weam/weam/public/js ./weam-multiplatform/public
+cp -r ./weam/weam/resources ./weam-multiplatform
+cp -r ./weam/weam/routes ./weam-multiplatform
 cp -r ./weam/weam/.env ./weam-multiplatform/.env
 
 cd ./weam-multiplatform
-git add *.*
+git add .
 git commit -m "commit"
 git push origin master
-
-cd ..
-rm -rf ./weam-multiplatform
--- weam/resources/views/welcome.blade.php
+++ weam/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello world</title>
+        <title>Test</title>
 
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
