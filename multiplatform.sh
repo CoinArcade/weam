db2d75e6e473a02d2392eb9852387a1a520d6b6a
Matéo Castella <castella.mateo@gmail.com>
2020-11-02 11:36:35 +0100

    update multiplatform.sh

---
+++ multiplatform.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /var/www/html/
+
+cp -r ./weam/weam/app ./weam-multiplatform/app
+cp -r ./weam/weam/public/css ./weam-multiplatform/public/css
+cp -r ./weam/weam/public/js ./weam-multiplatform/public/js
+cp -r ./weam/weam/resources ./weam-multiplatform/resources
+cp -r ./weam/weam/routes ./weam-multiplatform/routes
+cp -r ./weam/weam/.env ./weam-multiplatform/.env
+
+cd ./weam-multiplatform
+git add *.*
+git commit -m "commit"
+git push origin main
