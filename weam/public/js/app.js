2bb4d85739cc7e5214344e956fb7e47b2b58948d
Matéo Castella <castella.mateo@gmail.com>
2021-01-02 13:15:51 +0100

    login loading bar

--- weam/public/js/app.js
+++ weam/public/js/app.js
@@ -6471,6 +6471,48 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=script&lang=js&":
+/*!***********************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=script&lang=js& ***!
+  \***********************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: "ButtonLoaderComponent",
+  props: ['text'],
+  data: function data() {
+    return {
+      isLoading: 0,
+      savedText: ""
+    };
+  },
+  mounted: function mounted() {
+    this.savedText = this.text;
+  },
+  methods: {
+    loaderManager: function loaderManager() {
+      if (this.isLoading === 0) {
+        this.savedText = '<div class="loader-bar"><div></div><div></div><div></div></div>';
+        this.isLoading = 1;
+      } else {
+        this.savedText = this.text;
+        this.isLoading = 0;
+      }
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/general/swal/ChooseLanguageComponent.vue?vue&type=script&lang=js&":
 /*!***********************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/general/swal/ChooseLanguageComponent.vue?vue&type=script&lang=js& ***!
@@ -6561,6 +6603,7 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _loader_ButtonLoaderComponent__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../loader/ButtonLoaderComponent */ "./resources/js/components/general/loader/ButtonLoaderComponent.vue");
 //
 //
 //
@@ -6648,8 +6691,12 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "LoginAndSigninForm",
+  components: {
+    'ButtonLoader': _loader_ButtonLoaderComponent__WEBPACK_IMPORTED_MODULE_0__["default"]
+  },
   data: function data() {
     return {
       activetab: 1
@@ -34058,6 +34105,33 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e&":
+/*!***************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e& ***!
+  \***************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", {
+    domProps: { innerHTML: _vm._s(_vm.savedText) },
+    on: { click: _vm.loaderManager }
+  })
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/general/swal/ChooseLanguageComponent.vue?vue&type=template&id=063a92c0&":
 /*!***************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/general/swal/ChooseLanguageComponent.vue?vue&type=template&id=063a92c0& ***!
@@ -34267,13 +34341,8 @@ var render = function() {
                   staticClass:
                     "w-full flex-shrink-0 bg-th-color text-white text-base font-semibold py-1 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-th-btn focus:ring-offset-2 focus:ring-offset-th-btn-soft"
                 },
-                [
-                  _vm._v(
-                    "\n                " +
-                      _vm._s(_vm.__("Log In")) +
-                      "\n            "
-                  )
-                ]
+                [_c("button-loader", { attrs: { text: _vm.__("Log In") } })],
+                1
               )
             ])
           ]
@@ -34482,22 +34551,7 @@ var render = function() {
               })
             ]),
             _vm._v(" "),
-            _c("div", { staticClass: "w-full mt-5" }, [
-              _c(
-                "button",
-                {
-                  staticClass:
-                    "w-full flex-shrink-0 bg-th-color text-white text-base font-semibold py-1 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-th-btn focus:ring-offset-2 focus:ring-offset-th-btn-soft"
-                },
-                [
-                  _vm._v(
-                    "\n                " +
-                      _vm._s(_vm.__("Sign Up")) +
-                      "\n            "
-                  )
-                ]
-              )
-            ])
+            _vm._m(1)
           ]
         )
       : _vm._e()
@@ -34513,6 +34567,21 @@ var staticRenderFns = [
       { staticClass: "col-span-full md:col-start-2 md:col-span-10" },
       [_c("hr", { staticClass: "w-full" })]
     )
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "w-full mt-5" }, [
+      _c(
+        "button",
+        {
+          staticClass:
+            "w-full flex-shrink-0 bg-th-color text-white text-base font-semibold py-1 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-th-btn focus:ring-offset-2 focus:ring-offset-th-btn-soft"
+        },
+        [_vm._v("\n                test\n            ")]
+      )
+    ])
   }
 ]
 render._withStripped = true
@@ -50548,6 +50617,75 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AppComponent_vue_vue_type_template_id_cd268472___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AppComponent_vue_vue_type_template_id_cd268472___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ }),
+
+/***/ "./resources/js/components/general/loader/ButtonLoaderComponent.vue":
+/*!**************************************************************************!*\
+  !*** ./resources/js/components/general/loader/ButtonLoaderComponent.vue ***!
+  \**************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _ButtonLoaderComponent_vue_vue_type_template_id_7e013c8e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e& */ "./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e&");
+/* harmony import */ var _ButtonLoaderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ButtonLoaderComponent.vue?vue&type=script&lang=js& */ "./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _ButtonLoaderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _ButtonLoaderComponent_vue_vue_type_template_id_7e013c8e___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _ButtonLoaderComponent_vue_vue_type_template_id_7e013c8e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/general/loader/ButtonLoaderComponent.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=script&lang=js&":
+/*!***************************************************************************************************!*\
+  !*** ./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=script&lang=js& ***!
+  \***************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ButtonLoaderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../node_modules/babel-loader/lib??ref--4-0!../../../../../node_modules/vue-loader/lib??vue-loader-options!./ButtonLoaderComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ButtonLoaderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e&":
+/*!*********************************************************************************************************!*\
+  !*** ./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e& ***!
+  \*********************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ButtonLoaderComponent_vue_vue_type_template_id_7e013c8e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../../node_modules/vue-loader/lib??vue-loader-options!./ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/general/loader/ButtonLoaderComponent.vue?vue&type=template&id=7e013c8e&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ButtonLoaderComponent_vue_vue_type_template_id_7e013c8e___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ButtonLoaderComponent_vue_vue_type_template_id_7e013c8e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
--- weam/resources/js/components/general/swal/LoginAndSigninFormComponent.vue
+++ weam/resources/js/components/general/swal/LoginAndSigninFormComponent.vue
@@ -3,9 +3,9 @@
     <div class="grid grid-cols-12 gap-4">
 
         <div class="flex justify-between col-span-full md:col-start-2 md:col-span-10 tabs-content">
-            <button v-on:click="activetab=1" v-bind:class="[ activetab === 1 ? 'active' : '' ]" class="w-full cursor-pointer tab rounded-lg py-2 mx-2"><p class="text-center">
+            <button @click="activetab=1" :class="[ activetab === 1 ? 'active' : '' ]" class="w-full cursor-pointer tab rounded-lg py-2 mx-2"><p class="text-center">
                 {{ __('Log In') }}</p></button>
-            <button v-on:click="activetab=2" v-bind:class="[ activetab === 2 ? 'active' : '' ]" class="w-full cursor-pointer tab rounded-lg py-2 mx-2"><p class="text-center">{{ __('Sign Up') }}</p></button>
+            <button @click="activetab=2" :class="[ activetab === 2 ? 'active' : '' ]" class="w-full cursor-pointer tab rounded-lg py-2 mx-2"><p class="text-center">{{ __('Sign Up') }}</p></button>
         </div>
 
         <div class="col-span-full md:col-start-2 md:col-span-10">
@@ -17,19 +17,19 @@
                 <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2 text-left" for="login-username">
                     {{ __('Username') }}
                 </label>
-                <input id="login-username" type="text" v-bind:placeholder="__('Enter your username')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <input id="login-username" type="text" :placeholder="__('Enter your username')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 <p id="login-username-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-3">
                 <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2 text-left" for="login-password">
                     {{ __('Password') }}
                 </label>
-                <input id="login-password" type="password" v-bind:placeholder="__('************')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <input id="login-password" type="password" :placeholder="__('************')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 <p id="login-password-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-5">
                 <button class="w-full flex-shrink-0 bg-th-color text-white text-base font-semibold py-1 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-th-btn focus:ring-offset-2 focus:ring-offset-th-btn-soft">
-                    {{ __('Log In') }}
+                    <button-loader v-bind:text="__('Log In')"></button-loader>
                 </button>
             </div>
         </div>
@@ -39,28 +39,28 @@
                 <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2 text-left" for="signup-username">
                     {{ __('Username') }}
                 </label>
-                <input id="signup-username" type="text" v-bind:placeholder="__('Enter your username')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <input id="signup-username" type="text" :placeholder="__('Enter your username')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 <p id="signup-username-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-3">
                 <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2 text-left" for="signup-email">
                     {{ __('Email address') }}
                 </label>
-                <input id="signup-email" type="email" v-bind:placeholder="__('Enter your email address')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <input id="signup-email" type="email" :placeholder="__('Enter your email address')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 <p id="signup-email-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-3">
                 <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2 text-left" for="signup-password">
                     {{ __('Password') }}
                 </label>
-                <input id="signup-password" type="password" v-bind:placeholder="__('************')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <input id="signup-password" type="password" :placeholder="__('************')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 <p id="signup-password-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-3">
                 <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2 text-left" for="signup-password-confirm">
                     {{ __('Password confirmation') }}
                 </label>
-                <input id="signup-password-confirm" type="password" v-bind:placeholder="__('************')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <input id="signup-password-confirm" type="password" :placeholder="__('************')" class="bg-th-body flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 <p id="signup-password-confirm-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-3">
@@ -68,15 +68,15 @@
                     {{ __('Date of birth') }}
                 </label>
                 <div id="signup-birthdate" class="flex justify-between">
-                    <input id="signup-birthdate-day" type="text" v-bind:placeholder="__('Day')" class="bg-th-body mx-1 flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                    <input id="signup-birthdate-month" type="text" v-bind:placeholder="__('Month')" class="bg-th-body mx-1 flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                    <input id="signup-birthdate-year" type="text" v-bind:placeholder="__('Year')" class="bg-th-body mx-1 flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                    <input id="signup-birthdate-day" type="text" :placeholder="__('Day')" class="bg-th-body mx-1 flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                    <input id="signup-birthdate-month" type="text" :placeholder="__('Month')" class="bg-th-body mx-1 flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                    <input id="signup-birthdate-year" type="text" :placeholder="__('Year')" class="bg-th-body mx-1 flex-1 appearance-none border border-transparent w-full py-1 px-2 bg-grey-lighter text-grey-darker rounded-lg text-sm  focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                 </div>
                 <p id="signup-birthdate-error" class="text-red-600 text-sm text-left mt-2"></p>
             </div>
             <div class="w-full mt-5">
                 <button class="w-full flex-shrink-0 bg-th-color text-white text-base font-semibold py-1 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-th-btn focus:ring-offset-2 focus:ring-offset-th-btn-soft">
-                    {{ __('Sign Up') }}
+                    test
                 </button>
             </div>
         </div>
@@ -87,9 +87,15 @@
 
 <script>
 
+    import ButtonLoader from '../loader/ButtonLoaderComponent';
+
     export default {
 
         name: "LoginAndSigninForm",
+
+        components: {
+            'ButtonLoader': ButtonLoader
+        },
 
         data: function() {
 
